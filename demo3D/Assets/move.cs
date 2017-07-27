5dc20ec542e95971b43ac4b38e7732a8b1053515
woohyukkk <woo328968014@gmail.com>
2017-07-27 17:11:18 -0400

    demo added
    first demo

---
+++ demo3D/Assets/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	public GameObject G;
+	float x=-1;
+	bool f=true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//while(true){
+			//G.transform.Translate (0, x, 0);
+
+		//x = x*(-1);
+			//if (x > -0.25 && f == true)
+			//	x -= 0.01f;
+			//else if (x == -0.25)
+			///	f = false;
+			//else if(x< 0.3 && f==false)
+			//	x+=0.01f;
+			//else if (x == 0.3)
+			//	f = true;
+				
+		//}
+	}
+}
---
+++ demo3D/Assets/player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	public Animator A;
+	// Use this for initialization
+	void Start () {
+		A.Play(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
